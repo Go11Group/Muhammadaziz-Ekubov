75ad4030600c480a94691ab62a62c56f07a153d7
Mikegof <147698923+MuhammadAziz-GO@users.noreply.github.com>
2024-06-04 17:44:55 +0500

    Update course.go

--- 8-homework/storage/postgres/course.go
+++ 8-homework/storage/postgres/course.go
@@ -1,8 +1,8 @@
 package postgres
 
 import (
-	"database/sql"
 	"add/model"
+	"database/sql"
 )
 
 type CourseRepo struct {
@@ -14,6 +14,25 @@ func NewCourseRepo(DB *sql.DB) *CourseRepo {
 }
 
 func (c *CourseRepo) Create(course *model.Course) error {
+	_, err := c.DB.Exec("insert into course (id , name, field) values ($1, $2, $3)", course.Id, course.Name, course.Field)
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
+func (c *CourseRepo) Update(id int, name, field string) error {
+	_, err := c.DB.Exec("update course set name = $1, field = $2 where id = $3", name, field, id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *CourseRepo) Delete(id int, name string) error {
+	_, err := c.DB.Exec("delete from course where id = $1 and name = $2", id, name)
+	if err != nil {
+		return err
+	}
 	return nil
 }
